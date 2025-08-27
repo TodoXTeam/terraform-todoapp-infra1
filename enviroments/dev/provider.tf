b99d48796e2a0a9803913b73c35ba4d17ac50ae3
unknown <shankarsiddharth00@gmail.com>
2025-08-27 14:28:52 +0530

    Added Terraform infra code

---
+++ enviroments/dev/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.41.0"
+    }
+  }
+  #   backend "azurerm" {
+  #     resource_group_name  = ""
+  #     storage_account_name = ""
+  #     container_name       = ""
+  #     key                  = ""
+  #   }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "027bc23a-8a9d-41f9-b17f-65bf7ca72059"
+}
