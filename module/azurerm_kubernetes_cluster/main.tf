f110befbf4860d2b375cac9a0bdb78fb0b0242c2
unknown <shankarsiddharth00@gmail.com>
2025-09-24 15:29:31 +0530

    module

--- module/azurerm_kubernetes_cluster/main.tf
+++ module/azurerm_kubernetes_cluster/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_B2ms"
+    vm_size    = "Standard_B1ms"
   }
 
   identity {
