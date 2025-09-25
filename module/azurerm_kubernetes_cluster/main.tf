c91621a2543c198b4c998240b49f89dc4e89e016
unknown <shankarsiddharth00@gmail.com>
2025-09-25 05:38:10 +0530

    vm size change

--- module/azurerm_kubernetes_cluster/main.tf
+++ module/azurerm_kubernetes_cluster/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_B1ms"
+    vm_size    = "Standard_DS2_v2"
   }
 
   identity {
